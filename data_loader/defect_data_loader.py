96df7ac292ba832b7e067faa069d1247ebc91092
Admin <juryev7@gmail.com>
2022-07-04 16:53:33 +0300

    config abstract classes

--- data_loader/defect_data_loader.py
+++ data_loader/defect_data_loader.py
@@ -2,15 +2,19 @@ from tensorflow import keras
 import numpy as np
 from tensorflow.keras.preprocessing.image import load_img
 
+from base.base_data_loader import BaseDataLoader
 
-class OxfordPets(keras.utils.Sequence):
-    """Helper to iterate over the data (as Numpy arrays)."""
 
-    def __init__(self, config, batch_size, img_size, input_img_paths, target_img_paths):
-        self.batch_size = batch_size
-        self.img_size = img_size
-        self.input_img_paths = input_img_paths
-        self.target_img_paths = target_img_paths
+class ImageDataLoader(BaseDataLoader, keras.utils.Sequence):
+    """Helper to iterate over the data (as Numpy arrays).OxfordPets"""
+
+    def __init__(self, config):
+        super(ImageDataLoader, self).__init__(config)
+        super(BaseDataLoader, self).__init__(config)
+        self.batch_size = config.model.batch_size
+        self.img_size = config.model.img_size
+        self.input_img_paths = config.model.input_img_paths
+        self.target_img_paths = config.model.target_img_paths
 
     def __len__(self): # сколько батчей в выборке
         return len(self.target_img_paths) // self.batch_size
